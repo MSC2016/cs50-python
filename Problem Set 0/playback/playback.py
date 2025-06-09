9d0f108217d674697b77ff179f15c5dd81dac8fc
MSC2016 <17535858+MSC2016@users.noreply.github.com>
2025-06-09 12:22:56 +0100

    modified:   playback/playback.py

---
+++ Problem Set 0/playback/playback.py
@@ -0,0 +1,9 @@
+# create a playbsck function
+def playback():
+    # get the user input
+    text = input()
+    # replace spaces with ... and print
+    print(text.replace(' ','...'))
+
+# call the function
+playback()
