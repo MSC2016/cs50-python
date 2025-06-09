48460e3bb841bebd6adc8c69bbe709a324d7928f
MSC2016 <17535858+MSC2016@users.noreply.github.com>
2025-06-09 14:39:12 +0100

    modified:   faces/faces.py

---
+++ Problem Set 0/faces/faces.py
@@ -0,0 +1,18 @@
+# define main function
+def main():
+    # get user input
+    message = input("Enter message here: ")
+    
+    # call convert and assign result to output
+    output = convert(message)
+
+    # print output
+    print(output)
+
+# define convert functionm
+def convert(str):
+    # replace text and return it
+    return str.replace(":)","🙂").replace(":(","🙁")
+
+# call main function
+main()
