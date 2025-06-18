ebb4bb53f6fddc0dcf1fb0b1eec2d7e20a5dc5e3
MSC2016 <17535858+MSC2016@users.noreply.github.com>
2025-06-18 20:46:55 +0100

    new file:   game.py

---
+++ Problem Set 4/game/game.py
@@ -0,0 +1,48 @@
+import random
+
+
+def main():
+    # get a user input and assign it to level
+    level = get_user_input('Level: ')
+    # get a random number between 0 and level
+    number = random.randint(0, level)
+    # set the initial value of the guess to -1, to ensure the 
+    # while loop is executed
+    guess = -1
+    # loop until the user guesses the number
+    while guess != number:
+        # get the user guess
+        guess = get_user_input('Guess: ')
+        # if the guess is smaller print too small
+        if guess < number:
+            print('Too small!')
+        # if the guess is larger print too large
+        elif guess > number:
+            print('Too large!')
+    # otherwise, print just right and exit
+    print('Just right!')
+
+    
+def get_user_input(s='') -> int:
+    '''
+    Get an input from a user, 
+    if a value for s is given, it should be the prompt
+    '''
+    # loop forever
+    while True:
+        # try catch block
+        try:
+            # get an input from the user and convert to int
+            user_input = int(input(s))
+            # raise an exception if input is zero or less, forcing a reprompt
+            if user_input <= 1:
+                raise Exception
+            # if successfull, return it
+            return user_input
+        # catch all, and retry
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    main()
